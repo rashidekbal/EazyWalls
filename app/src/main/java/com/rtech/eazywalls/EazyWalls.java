649a5ac0206c7631ac18902642343a09207cd0dc
Rasid Ekbal <rtechdevlopment123@gmail.com>
2025-12-19 14:19:43 +0530

    initial commit

---
+++ app/src/main/java/com/rtech/eazywalls/EazyWalls.java
@@ -0,0 +1,20 @@
+package com.rtech.eazywalls;
+
+import android.app.Application;
+import android.content.Context;
+
+public class EazyWalls extends Application {
+    private static EazyWalls instance;
+
+    @Override
+    public void onCreate() {
+        instance=this;
+        super.onCreate();
+    }
+    public static EazyWalls getInstance(){
+        return instance;
+    }
+    public static Context getContext(){
+        return instance.getApplicationContext();
+    }
+}
